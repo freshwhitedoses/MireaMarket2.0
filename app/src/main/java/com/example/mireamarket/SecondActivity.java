f621259dad6da8dc1df24a1df28e93a12f5992b8
olya <olenkaevstratova@gmail.com>
2022-04-21 16:11:56 +0300

    commit4

--- app/src/main/java/com/example/mireamarket/SecondActivity.java
+++ app/src/main/java/com/example/mireamarket/SecondActivity.java
@@ -30,7 +30,7 @@ public class SecondActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_second);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
