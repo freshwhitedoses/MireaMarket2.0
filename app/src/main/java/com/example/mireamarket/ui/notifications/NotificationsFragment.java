62f526a5b4e3d43df9c17c8c680bad255ff641d8
olya <olenkaevstratova@gmail.com>
2022-05-06 21:32:54 +0300

    test

--- app/src/main/java/com/example/mireamarket/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/mireamarket/ui/notifications/NotificationsFragment.java
@@ -1,35 +1,42 @@
 package com.example.mireamarket.ui.notifications;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import com.example.mireamarket.R;
+import com.example.mireamarket.SecondActivity;
 import com.example.mireamarket.databinding.FragmentNotificationsBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NotificationsFragment extends Fragment {
 
     private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
-
-    Button logout_btn;
-    SharedPreferences sharedPreferences;
-
-    private static final String SHARED_PREF_NAME = "mypref";
-    private static final String KEY_EMAIL = "name";
-    private static final String KEY_PASSWORD = "email";
+    private String checker="";
+    private Uri imageUri;
+    private CircleImageView profileImView;
+    private EditText name, surname, group;
+    private TextView saveProfile, closeProfile;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -38,31 +45,55 @@ public class NotificationsFragment extends Fragment {
 
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        profileImView=root.findViewById(R.id.account_image);
+        name=root.findViewById(R.id.fill_name);
+        surname=root.findViewById(R.id.fill_surname);
+        group=root.findViewById(R.id.fill_group);
+        saveProfile=root.findViewById(R.id.save_account_tw);
+        closeProfile=root.findViewById(R.id.close_account_tw);
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-
-        logout_btn = root.findViewById(R.id.logout);
-        sharedPreferences = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
-
-
-        logout_btn.setOnClickListener(new View.OnClickListener() {
+        saveProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.clear();
-                editor.commit();
-                Toast.makeText(getActivity(), "Вы вышли из аккаунта", Toast.LENGTH_SHORT).show();
-                getActivity().finish();
+                if(checker.equals("clicked")){
+                    return;
+                }
+                else{
+                    updateOnlyUserInfo();
+                }
             }
         });
+
+        profileImView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checker="clicked";
+                CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                        .start(new SecondActivity());
+
+
+            }
+        });
+
+
+
+
+
         return root;
+
+
     }
+
+    private void updateOnlyUserInfo() {
+        DatabaseReference ref=FirebaseDatabase.getInstance().getReference().child("Users");
+        HashMap<String,Object> usermap=new HashMap<>();
+        usermap.put("name",name.getText().toString());
+        usermap.put("surname",surname.getText().toString());
+        usermap.put("group",group.getText().toString());
+        ref.child(new)
+    }
+
 
     @Override
     public void onDestroyView() {
