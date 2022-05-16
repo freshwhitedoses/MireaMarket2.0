ab07a4010cd7401939ac057e25c4b356d13a33c8
olya <olenkaevstratova@gmail.com>
2022-05-16 17:50:55 +0300

    local cabinet

--- app/src/main/java/com/example/mireamarket/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/mireamarket/ui/notifications/NotificationsFragment.java
@@ -20,11 +20,18 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mireamarket.Moduls.User;
 import com.example.mireamarket.R;
 import com.example.mireamarket.SecondActivity;
 import com.example.mireamarket.databinding.FragmentNotificationsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.util.HashMap;
@@ -33,7 +40,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class NotificationsFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+        private NotificationsViewModel notificationsViewModel;
     private FragmentNotificationsBinding binding;
     private String checker="";
     private Uri imageUri;
@@ -41,6 +48,7 @@ public class NotificationsFragment extends Fragment {
     private EditText name, surname, group;
     private TextView saveProfile, closeProfile;
     Button logout;
+    private static User user;
     SharedPreferences sharedPreferences;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -60,6 +68,30 @@ public class NotificationsFragment extends Fragment {
 
         sharedPreferences = getActivity().getSharedPreferences("pref", Context.MODE_PRIVATE);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String uid = user.getUid();
+        DatabaseReference users = database.getReference("Users");
+        DatabaseReference uidRef = users.child(uid);
+
+        uidRef.addValueEventListener(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        if (user != null) {
+                            name.setText(user.getName());
+                            surname.setText(user.getSurname());
+                            group.setText(user.getGroup());
+                        }
+                        NotificationsFragment.user = user;
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,6 +100,9 @@ public class NotificationsFragment extends Fragment {
                 editor.commit();
                 Toast.makeText(getActivity(), "Log out successfully", Toast.LENGTH_SHORT).show();
                 getActivity().finish();
+
+
+                //name=getDisplayName();
             }
         });
 
